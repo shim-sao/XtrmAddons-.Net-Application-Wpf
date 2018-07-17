34e66642e2395842a5e50c4051b9bb363890c104
Laurent Barraud <shimsao@hotmail.com>
2018-07-17 21:16:59 +0200

    Under construction

--- Fotootof/XtrmAddons.Fotootof/Properties/AssemblyInfo.cs
+++ Fotootof/XtrmAddons.Fotootof/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.18198.1546")]
-[assembly: AssemblyFileVersion("1.0.18198.1546")]
+[assembly: AssemblyVersion("1.0.18198.1908")]
+[assembly: AssemblyFileVersion("1.0.18198.1908")]
