cfe42c4cccf3685b3453c0ac27331ab6f60e10cd
Laurent Barraud <shimsao@hotmail.com>
2018-07-21 16:55:21 +0200

    Under construction

--- Fotootof/XtrmAddons.Fotootof.Lib.SQLite/Properties/AssemblyInfo.cs
+++ Fotootof/XtrmAddons.Fotootof.Lib.SQLite/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.18.0720.2249")]
-[assembly: AssemblyFileVersion("1.18.0720.2249")]
+[assembly: AssemblyVersion("1.18.0721.1439")]
+[assembly: AssemblyFileVersion("1.18.0721.1439")]
