6d75088d60bb93f2ef15f5ea17be54d8305b5bda
Shim-Sao <35068017+shim-sao@users.noreply.github.com>
2019-05-29 17:04:45 +0200

    Update build

--- Fotootof/Fotootof/Properties/AssemblyInfo.cs
+++ Fotootof/Fotootof/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Windows;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.19149.1416")]
-[assembly: AssemblyFileVersion("1.3.19149.1416")]
+[assembly: AssemblyVersion("1.3.19149.1422")]
+[assembly: AssemblyFileVersion("1.3.19149.1422")]
