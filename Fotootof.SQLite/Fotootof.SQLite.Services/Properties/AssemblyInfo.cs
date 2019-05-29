fd7782088374fa57c072a5a4ee0d88cbde73abcb
Shim-Sao <35068017+shim-sao@users.noreply.github.com>
2019-05-29 15:11:12 +0200

    Update nuggets

--- Fotootof.SQLite/Fotootof.SQLite.Services/Properties/AssemblyInfo.cs
+++ Fotootof.SQLite/Fotootof.SQLite.Services/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1901.19149.1217")]
-[assembly: AssemblyFileVersion("1.1901.19149.1217")]
+[assembly: AssemblyVersion("1.1901.19149.1223")]
+[assembly: AssemblyFileVersion("1.1901.19149.1223")]
